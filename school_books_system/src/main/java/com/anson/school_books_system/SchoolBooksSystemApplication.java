a9c16a534befdd5b405c2aed3d97b641ffd16743
AnsonYALin <chistine1018@gmail.com>
2024-07-29 15:22:38 +0800

    Init backend project.

---
+++ school_books_system/src/main/java/com/anson/school_books_system/SchoolBooksSystemApplication.java
@@ -0,0 +1,13 @@
+package com.anson.school_books_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBooksSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolBooksSystemApplication.class, args);
+    }
+
+}
