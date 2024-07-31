06b2b37d2e4bdbae84dce994fa47b35eab40fa3f
AnsonYALin <chistine1018@gmail.com>
2024-07-31 21:47:14 +0800

    Feature: Adjust checkphone rule.

--- school_books_system/src/main/java/com/anson/school_books_system/utils/SysUtil.java
+++ school_books_system/src/main/java/com/anson/school_books_system/utils/SysUtil.java
@@ -17,7 +17,11 @@ public class SysUtil {
     /**
      * 手機號碼正則
      */
-    public final static String PHONE_REGEX = "^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}$";
+    // 大陸
+    // public final static String PHONE_REGEX = "^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}$";
+    // 台灣
+    public final static String PHONE_REGEX = "^09\\d{8}$";
+
 
 
     /**
